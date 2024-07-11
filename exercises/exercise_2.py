bbe10ff585a307c195fdaebe3548a1ecfcc5904f
patlikespython <patryktolkin9@gmail.com>
2024-07-11 11:06:37 +0100

    Update exercise_2.py

--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,7 +1,7 @@
 # Your solution to Exercise 2
 to_check=(input("Enter languages with spaces"))
 to_check.split()
-reversed= langauges[::-1]
+reversed= to_check[::-1]
 reverse=sorted(reversed)[::-1]
 print(reverse)
 
